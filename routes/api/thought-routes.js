3022d6cf695f5ee9b366e33c0efa728f6eb6a29d
tarequem <moatartareque@gmail.com>
2022-09-26 10:53:49 -0400

    fixed error with CRUD labels

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -2,22 +2,22 @@ const router = require('express').Router();
 const {
     getAllThoughts,
     getThoughtById,
-    addThought,
+    createThought,
     updateThought,
-    removeThought,
+    deleteThought,
     addReaction,
-    removeReaction
+    deleteReaction
 } = require('../../controllers/thought-controller');
 
 //thought routes
 router.route('/').get(getAllThoughts);
 router.route('/:thoughtId').get(getThoughtById);
-router.route('/').post(addThought);
+router.route('/').post(createThought);
 router.route('/:thoughtId').put(updateThought);
-router.route('/:thoughtId').delete(removeThought);
+router.route('/:thoughtId').delete(deleteThought);
 
 //reaction routes
 router.route('/:thoughtId/reactions').post(addReaction)
-router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
